fd96aeb3932691a8c14c2a060567c7c88aa26cbb
lmercado <lmercado@ignetworks.com>
2023-02-15 14:27:23 +0000

    [LM]redirect and template

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    url_for,
+)
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return "<h6>Home</h6>"
 
 @app.route("/saludo/<nombre>")
 def saludo(nombre):
@@ -13,3 +18,11 @@ def saludo(nombre):
 @app.route("/edad/<int:edad>")
 def get_edad(edad):
     return f'<h3> Tu edad es: {edad}</h3>'
+
+@app.route("/mostrar/<nombre>",methods=['GET', 'POST'])
+def get_mostrar(nombre):
+    return render_template('mostrar.html', nombre_parametro=nombre)
+
+@app.route("/redirect")
+def get_redirect():
+    return redirect(url_for('home'))
